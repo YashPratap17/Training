f7ab470dc1520841b62a3ad112ca4550d30d40ba
Yash Pratap Singh <yashpsingh17@gmail.com>
2026-07-02 14:12:03 +0530

    data types

---
+++ datatypes.py
@@ -0,0 +1,38 @@
+# print("Hello World!")
+
+
+# # bool
+# b = True
+# print(type(b))
+
+# a = True 
+# b = False
+# print(a+b)
+
+# #str
+
+# s1 = 'yashpratapsingh'
+# print(type(s1))
+# print(s1[0:4])
+# print(s1[4:10])
+# print(s1[10:])
+# print(s1[::-1])
+
+
+#Type Casting
+#int()
+
+# print(int(516.21))
+# print(int("Yash")) Error
+# print(int(5+5j)) Error
+# print(int(True))
+# print(int(False))
+
+#Float
+
+print(float(5))
+print(float(516.21))
+# print(float("Yash")) 
+# print(float(5+5j))
+print(float(True))
+print(float(False))

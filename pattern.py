7c705a51a11129de08a574e91fab5a05ed82a28d
Yash Pratap Singh <yashpsingh17@gmail.com>
2026-07-07 10:45:06 +0530

    Pattern and File handling

---
+++ pattern.py
@@ -0,0 +1,14 @@
+# row = 5
+# for i in range(row,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+row = 5 
+for k in range(row,0,-1):
+    for i in range(1,row):
+        for i in range(i):
+            print("*",end=" ")
+        print()
+    print(end=" ")
